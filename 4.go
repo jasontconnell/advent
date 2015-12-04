20ab511f6edf34156f36246d00205302c544128a
Jason Connell <jconnell@delphicdigital.com>
2015-12-04 09:08:53 -0500

    Day 4 of advent of code.

---
+++ 4.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"crypto/md5"
+	"strings"
+	"strconv"
+	"io"
+)
+
+var input = "iwrupvqb"
+
+func main() {
+	found := false
+	lastTest := ""
+	for i := 0; !found; i++ {
+		test := input + strconv.Itoa(i)
+
+		hex := getMD5Hex(test)
+
+		found = strings.HasPrefix(hex, "000000")
+		if found {
+			lastTest = test
+		}
+	}
+
+	fmt.Println(lastTest)
+}
+
+func getMD5Hex(str string) string {
+	h := md5.New()
+	io.WriteString(h, str)
+	hex := fmt.Sprintf("%x", h.Sum(nil))
+	return hex
+}
