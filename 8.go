e444c2a7b0d65a9493aa94283d10d7e421096131
Jason Connell <jasontconnell@gmail.com>
2015-12-08 00:43:08 -0500

    Day 8 solution

---
+++ 8.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"regexp"
+	//"strconv"
+	"strings"
+)
+
+var input = "8.txt"
+
+func main(){
+	if f,err := os.Open(input); err == nil {
+		scanner := bufio.NewScanner(f)
+
+		hex,_ := regexp.Compile(`\\x[abcdef0-9]{2}`)
+		q,_ := regexp.Compile(`\\"`)
+		bs,_ := regexp.Compile(`\\\\`)
+
+		total := 0
+		replaced := 0
+		for scanner.Scan() {
+			var txt = scanner.Text()
+			debug := false
+			if strings.Contains(txt, `\x`){
+				debug = true
+				fmt.Println(txt)
+			}
+			total += len(txt)
+			txt = strings.Trim(txt, `"`)
+			txt = bs.ReplaceAllString(txt, `\`)
+			txt = q.ReplaceAllString(txt, `"`)
+			txt = hex.ReplaceAllString(txt, ".")
+		
+			if debug {
+				fmt.Println(txt)
+			}
+			replaced += len(txt)
+		}
+		fmt.Println(total - replaced)
+	}
+}
