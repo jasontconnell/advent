3c016172b6d9750a2c847365db6876ed71d3afee
Jason Connell <jconnell@delphicdigital.com>
2015-12-17 13:34:39 -0500

    Day 17 solution

--- 17.go
+++ 17.go
@@ -32,118 +32,33 @@ func main() {
 
 		
 		fmt.Println(ary)
-		buckets := 0
+		
+		buckets := count(ary, 150, len(ary), 0)
 
-		combos := Combinate(ary, 4) // n choose r for 20,10 should be 184756
-		fmt.Println(combos)
-		fmt.Println(len(combos))
+		result := 0
+		i := 0
+		for result == 0 {
+			result = count(ary, 150, i, 0)
+			i++
+		}
 
-		fmt.Println(buckets)
+		fmt.Println("buckets", buckets)
+		fmt.Println("result", result)
 	}
 
 	fmt.Println("Time", time.Since(startTime))
 }
 
-func Combinate(ints []int, size int) (combos [][]int) {
-	loops := int(factorial(len(ints)) / (factorial(len(ints)-size) * factorial(size)))
-	
-	indices := []int{}
-	for i := 0; i < size; i++ {
-		indices = append(indices, i)
-	}
+func count(list []int, total, n, i int) int {
+	if i < 0 { i = 0 }
 
-	curindex := size-1
-	curindexmax := len(ints)-1
-
-	for i := 0; i < loops; i++ {
-		combo := []int{}
-		for _,index := range indices {
-			combo = append(combo, ints[index])
-		}
-
-		fmt.Println(combo)
-		curindex--
-
-		if curindex < 0 { curindex = size-1 }
-		
-
-		fmt.Println(curindex)
-
-		for indices[curindex] == len(ints) - 1 && curindexmax >= 0 {
-			curindexmax--
-		}
-
-		if curindex > 0 {
-			indices[curindex]++
-		}
-	}
-
-	fmt.Println(ints)
-
-	return
-}
-
-func factorial(n int) int64 {
-	if n == 1 || n == 2 {
-		return int64(n)
+	if n < 0 {
+		return 0
+	} else if total == 0 {
+		return 1
+	} else if i == len(list) || total < 0 {
+		return 0
 	} else {
-		return int64(n) * factorial(n-1)
+		return count(list, total, n, i+1) + count(list, total-list[i], n-1, i+1)
 	}
 }
-
-
-func Permutate(ints []int) [][]int {
-	var ret [][]int
-
-	if len(ints) == 2 {
-		ret = append(ret, []int{ ints[0], ints[1] })
-		ret = append(ret, []int{ ints[1], ints[0] })
-	} else {
-
-		for i := 0; i < len(ints); i++ {
-			strc := make([]int, len(ints))
-			copy(strc, ints)
-
-			t := strc[i]
-			sh := append(strc[:i], strc[i+1:]...)
-			perm := Permutate(sh)
-			
-			for _,p := range perm {
-				p = append([]int{ t }, p...)
-				ret = append(ret, p)
-			}
-		}
-	}
-
-	return ret
-}
-
-func Perms(total, num int) [][]int {
-	ret := [][]int{}
-
-	if num == 2 {
-		for i := 0; i < total/2 + 1; i++ {
-			ret = append(ret, []int{ total-i, i })
-			if i != total - i {
-				ret = append(ret, []int{ i, total-i })
-			}
-		}
-	} else {
-		for i := 0; i <= total; i++ {
-			perms := Perms(total-i, num-1)
-			for _, p := range perms {
-				q := append([]int{ i }, p...)
-				ret = append(ret, q)
-			}
-		}
-	}
-	return ret
-}
-
-
-// reg := regexp.MustCompile("-?[0-9]+")
-/* 			
-if groups := reg.FindStringSubmatch(txt); groups != nil && len(groups) > 1 {
-				fmt.Println(groups[1:])
-			}
-			*/
