968fcabe0ab1c43e41084994e0107cd430c4f75d
Jason Connell <jconnell@delphicdigital.com>
2016-12-31 03:07:29 -0500

    Day 14 part 2 solution

--- 2016/14.go
+++ 2016/14.go
@@ -7,7 +7,7 @@ import (
     "strconv"
     "strings"
 )
-var input = "abc"
+var input = "jlmsuwbz"
 
 type Key struct {
     Index int
@@ -20,32 +20,46 @@ func (key Key) String() string {
     return fmt.Sprintf("%v) Value: %v with Seed: %v .  Repeated char: %v", key.Index, key.Value, key.Seed, string(key.Char))
 }
 
+func getMap(start, count int, part2 bool) map[string]string {
+    mp := make(map[string]string)
+    for hk := 0; len(mp) < count; hk++ {
+        s := input + strconv.Itoa(hk)
+        md5 := MD5s(s)
+
+        if part2 {
+            for i := 0; i < 2016; i++ {
+                md5 = MD5s(md5)
+            }
+        }
+        if rep,_ := hasRepeat(md5, 3); rep {
+            mp[s] = md5
+        }
+    }
+    return mp
+}
+
 func main() {
     startTime := time.Now()
     i := 0
     keys := []Key{}
     part2 := true
 
+    hashes := getMap(0, 5000, part2)
+
     for len(keys) < 64 {
         s := input + strconv.Itoa(i)
-        md5 := MD5s(s)
+        if md5, ok := hashes[s]; ok {
+            if rep, char := hasRepeat(md5, 3); rep {
+                k := Key{ Seed: s, Value: md5, Index: i, Char: char }
 
-        if part2 {
-            for stretched := 0; stretched < 2016; stretched++ {
-                md5 = MD5s(md5)
-            }
-        }
-
-        if rep, char := hasRepeat(md5, 3); rep {
-            k := Key{ Seed: s, Value: md5, Index: i, Char: char }
-
-            for si := i+1; si < i+1000; si++ {
-                s2 := input + strconv.Itoa(si)
-                md52 := MD5s(s2)
-                if hasCharRepeat(md52, 5, char) {
-                    keys = append(keys, k)
-                    fmt.Println("got a key", k)
-                    break
+                for si := i+1; si <= i+1001; si++ {
+                    s2 := input + strconv.Itoa(si)
+                    if md52,ok := hashes[s2]; ok {
+                        if hasCharRepeat(md52, 5, char) {
+                            keys = append(keys, k)
+                            break
+                        }
+                    }
                 }
             }
         }
@@ -67,12 +81,12 @@ func hasCharRepeat(input string, l int, char rune) bool {
 
 func hasRepeat(input string, l int) (val bool, char rune ) {
     val = false
-    for i := 0; i < len(input) - l && !val; i++ {
+    for i := 0; i < len(input) - l + 1 && !val; i++ {
         for j := 1; j < l; j++ {
             if input[i] != input[i+j] { break }
-            if j == l - 1 { 
-                val = true
+            if j == l-1 { 
                 char = rune(input[i])
+                val = true //!hasCharRepeat(input, l+1, char)
             }
         }
     }
