6be4f884c773391a6d7a3165a30df3c8b502dab8
Jason Connell <jconnell@delphicdigital.com>
2017-12-03 04:05:03 -0500

    Go fmt

--- 2017/02.go
+++ 2017/02.go
@@ -1,104 +1,105 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "time"
-    "regexp"
-    "strconv"
-    "sort"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"time"
 )
+
 var input = "02.txt"
 
 var reg = regexp.MustCompile("-?([0-9]+)")
 
 func main() {
-    startTime := time.Now()
+	startTime := time.Now()
 
-    f, err := os.Open(input)
+	f, err := os.Open(input)
 
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    
-    scanner := bufio.NewScanner(f)
-    lines := []string{}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    for scanner.Scan() {
-        var txt = scanner.Text()
-        lines = append(lines, txt)
-    }
+	scanner := bufio.NewScanner(f)
+	lines := []string{}
 
-    ch := getChecksum(lines)
-    d := getDivisionResult(lines)
+	for scanner.Scan() {
+		var txt = scanner.Text()
+		lines = append(lines, txt)
+	}
 
-    fmt.Println("checksum", ch)
-    fmt.Println("division", d)
+	ch := getChecksum(lines)
+	d := getDivisionResult(lines)
 
-    fmt.Println("Time", time.Since(startTime))
+	fmt.Println("checksum", ch)
+	fmt.Println("division", d)
+
+	fmt.Println("Time", time.Since(startTime))
 }
 
 func getChecksum(lines []string) int {
-    sum := 0
+	sum := 0
 
-    for _, line := range lines {
-        min, max := getMinMax(line)
-        sum += (max-min)
-    }
+	for _, line := range lines {
+		min, max := getMinMax(line)
+		sum += (max - min)
+	}
 
-    return sum
+	return sum
 }
 
 func getDivisionResult(lines []string) int {
-    res := 0
+	res := 0
 
-    for _, line := range lines {
-        div := getDivision(line)
-        res += div
-    }
+	for _, line := range lines {
+		div := getDivision(line)
+		res += div
+	}
 
-    return res
+	return res
 }
 
 func getNums(line string) []int {
-    nums := []int{}
-    if groups := reg.FindAllStringSubmatch(line, -1); groups != nil && len(groups) > 1 {
-        for _, g := range groups {
-            val, err := strconv.Atoi(g[0])
-            if err != nil {
-                fmt.Println("parsing", err)
-            }
+	nums := []int{}
+	if groups := reg.FindAllStringSubmatch(line, -1); groups != nil && len(groups) > 1 {
+		for _, g := range groups {
+			val, err := strconv.Atoi(g[0])
+			if err != nil {
+				fmt.Println("parsing", err)
+			}
 
-            nums = append(nums, val)
-        }
-    }
+			nums = append(nums, val)
+		}
+	}
 
-    sort.Ints(nums)
+	sort.Ints(nums)
 
-    return nums
+	return nums
 }
 
-func getMinMax(line string) (min int, max int){
-    nums := getNums(line)
+func getMinMax(line string) (min int, max int) {
+	nums := getNums(line)
 
-    return nums[0], nums[len(nums)-1]
+	return nums[0], nums[len(nums)-1]
 }
 
-func getDivision(line string) (div int){
-    nums := getNums(line)
+func getDivision(line string) (div int) {
+	nums := getNums(line)
 
-    for i := 0; i < len(nums); i++ {
-        for j := len(nums)-1; j >= 0; j-- {
-            if i == j {
-                continue
-            }
-            if nums[j] % nums[i] == 0 {
-                return nums[j] / nums[i]
-            }
-        }
-    }
+	for i := 0; i < len(nums); i++ {
+		for j := len(nums) - 1; j >= 0; j-- {
+			if i == j {
+				continue
+			}
+			if nums[j]%nums[i] == 0 {
+				return nums[j] / nums[i]
+			}
+		}
+	}
 
-    return 0
+	return 0
 }
--- 2017/03.go
+++ 2017/03.go
@@ -1,146 +1,147 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "math"
+	"fmt"
+	"math"
+	"time"
 )
 
 var input = 368078
 
 func main() {
-    startTime := time.Now()
+	startTime := time.Now()
 
-    d := dist(input)
-    fmt.Println("distance", d)
+	d := dist(input)
+	fmt.Println("distance", d)
 
-    first := fillGrid(input)
-    fmt.Println("first filled larger than input", first)
+	first := fillGrid(input)
+	fmt.Println("first filled larger than input", first)
 
-    fmt.Println("Time", time.Since(startTime))
+	fmt.Println("Time", time.Since(startTime))
 }
 
 func ring(i int) int {
-    if i == 1 {
-        return 1
-    }
-    s := 1
-    itr := 1
+	if i == 1 {
+		return 1
+	}
+	s := 1
+	itr := 1
 
-    for (s*s) < i {
-        s += 2
-        itr++
-    }
+	for (s * s) < i {
+		s += 2
+		itr++
+	}
 
-    return s-2
+	return s - 2
 }
 
 func coords(i int) (int, int, int) {
-    sq := ring(i)
-    start := sq*sq + 1
-    max := sq+1
-    startx, starty := max, max-1
+	sq := ring(i)
+	start := sq*sq + 1
+	max := sq + 1
+	startx, starty := max, max-1
 
-    diff := i - start
-    turns := 0
+	diff := i - start
+	turns := 0
 
-    extra := diff
-    if diff > max - 1 {
-        for j := 0; j < 4; j++ {
-            m := max
-            if j == 0 { m = m - 1}
+	extra := diff
+	if diff > max-1 {
+		for j := 0; j < 4; j++ {
+			m := max
+			if j == 0 {
+				m = m - 1
+			}
 
-            if extra - m >= 0 {
-                extra = extra - m
-                turns ++
-            }
-        }
-    }
+			if extra-m >= 0 {
+				extra = extra - m
+				turns++
+			}
+		}
+	}
 
-    x, y := 0, 0
-    switch turns {
-        case 0: 
-            x = max
-            y = starty - extra
-        case 1: 
-            x = startx - extra
-            y = 0
-        case 2: 
-            x = 0
-            y = extra
-        case 3: 
-            x = extra
-            y = max
-    }
+	x, y := 0, 0
+	switch turns {
+	case 0:
+		x = max
+		y = starty - extra
+	case 1:
+		x = startx - extra
+		y = 0
+	case 2:
+		x = 0
+		y = extra
+	case 3:
+		x = extra
+		y = max
+	}
 
-    return x, y, max
+	return x, y, max
 }
 
 func dist(i int) float64 {
-    x, y, max := coords(i)
-    one := max / 2 // position of 1
+	x, y, max := coords(i)
+	one := max / 2 // position of 1
 
-    return math.Abs(float64(x - one)) + math.Abs(float64(y - one))
+	return math.Abs(float64(x-one)) + math.Abs(float64(y-one))
 }
 
 func fillGrid(d int) int {
-    val := 0
-    size := 1000
-    m := [][]int{}
+	val := 0
+	size := 1000
+	m := [][]int{}
 
-    startx, starty := size/2, size/2
+	startx, starty := size/2, size/2
 
-    m = make([][]int, size)
+	m = make([][]int, size)
 
-    for i := 0; i < size; i++ {
-        m[i] = make([]int, size)
-    }
+	for i := 0; i < size; i++ {
+		m[i] = make([]int, size)
+	}
 
-    xdir, ydir := 1, 0
+	xdir, ydir := 1, 0
 
-    curx := startx
-    cury := starty
+	curx := startx
+	cury := starty
 
-    m[cury][curx] = 1
+	m[cury][curx] = 1
 
+	//
+	for i := 0; i < size*size && val == 0; i++ {
+		curx = curx + xdir
+		cury = cury + ydir
 
-    // 
-    for i := 0; i < size*size && val == 0; i++ {
-        curx = curx + xdir
-        cury = cury + ydir
+		dur, dul, ddr, ddl := m[cury-1][curx+1], m[cury-1][curx-1], m[cury+1][curx+1], m[cury+1][curx-1]
+		up, down, left, right := m[cury-1][curx], m[cury+1][curx], m[cury][curx-1], m[cury][curx+1]
+		m[cury][curx] = dur + dul + ddr + ddl + up + down + left + right
 
-        dur, dul, ddr, ddl := m[cury-1][curx+1], m[cury-1][curx-1], m[cury+1][curx+1], m[cury+1][curx-1]
-        up,down,left,right := m[cury-1][curx], m[cury+1][curx], m[cury][curx-1], m[cury][curx+1]
-        m[cury][curx] = dur+dul+ddr+ddl+up+down+left+right
+		if m[cury][curx] > d {
+			val = m[cury][curx]
+			break
+		}
 
-        if m[cury][curx] > d {
-            val = m[cury][curx]
-            break
-        }
+		// turn??
+		if xdir == 1 { // going right. if up is 0, turn up
+			if up == 0 {
+				xdir = 0
+				ydir = -1
+			}
+		} else if xdir == -1 { // going left. if down is 0, turn down
+			if down == 0 {
+				xdir = 0
+				ydir = 1
+			}
+		} else if ydir == 1 { // going down. if right is 0, turn right
+			if right == 0 {
+				xdir = 1
+				ydir = 0
+			}
+		} else if ydir == -1 { // going up. if left is 0, turn left
+			if left == 0 {
+				xdir = -1
+				ydir = 0
+			}
+		}
+	}
 
-        // turn??
-        if xdir == 1 { // going right. if up is 0, turn up
-            if up == 0 { 
-                xdir = 0
-                ydir = -1
-            }
-        } else if xdir == -1 { // going left. if down is 0, turn down
-            if down == 0 {
-                xdir = 0
-                ydir = 1
-            }
-        } else if ydir == 1 { // going down. if right is 0, turn right
-            if right == 0 {
-                xdir = 1
-                ydir = 0
-            }
-        } else if ydir == -1 { // going up. if left is 0, turn left
-            if left == 0 {
-                xdir = -1
-                ydir = 0
-            }
-        }
-    }
-
-    return val
+	return val
 }
