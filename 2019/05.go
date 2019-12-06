eb6c6539e9ee51348fce3fabf07c04802de5a2a0
Jason Connell <jconnell@delphicdigital.com>
2019-12-05 23:01:00 -0500

    return value is not used, part 1 note, clean up

--- 2019/05.go
+++ 2019/05.go
@@ -26,9 +26,7 @@ func main() {
 
 	opcodes := []int{}
 	if scanner.Scan() {
-		//var txt = "3,9,8,9,10,9,4,9,99,-1,8"
 		var txt = scanner.Text()
-		// var txt = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
 		sopcodes := strings.Split(txt, ",")
 		for _, s := range sopcodes {
 			i, err := strconv.Atoi(s)
@@ -41,9 +39,8 @@ func main() {
 		}
 	}
 
-	ret := intcode.Exec(opcodes)
-
-	fmt.Println("Part 2: ", ret)
+	fmt.Println("Part 1 is achieved by disabled op codes 5,6,7,8")
+	intcode.Exec(opcodes)
 
 	fmt.Println("Time", time.Since(startTime))
 }
