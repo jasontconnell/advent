c5bf47e2486b56226fa223535718b853a488d69d
Jason Connell <jconnell@delphicdigital.com>
2020-12-04 14:53:12 -0500

    Saving work from 2019, starting 2020

--- 2019/14.go
+++ 2019/14.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-var input = "14.txt"
+var input = "14_test.txt"
 
 type reaction struct {
 	output chemical
@@ -18,7 +18,7 @@ type reaction struct {
 
 type chemical struct {
 	name  string
-	count int
+	count int64
 }
 
 func main() {
@@ -43,7 +43,7 @@ func main() {
 		for _, r3 := range r2 {
 			r4 := strings.Fields(r3)
 
-			c, err := strconv.Atoi(r4[0])
+			c, err := strconv.ParseInt(r4[0], 10, 64)
 			if err != nil {
 				fmt.Println("error parsing", err)
 			}
@@ -54,7 +54,7 @@ func main() {
 		}
 
 		r5 := strings.Fields(rs[1])
-		c, err := strconv.Atoi(r5[0])
+		c, err := strconv.ParseInt(r5[0], 10, 64)
 		if err != nil {
 			fmt.Println("parsing out", err)
 		}
@@ -72,37 +72,95 @@ func main() {
 	p1 := part1(cp, 1)
 	fmt.Println("Part 1: ", p1)
 
+	p2 := part2(cp, p1) // 1_000_000_000_000)
+	fmt.Println("Part 2: ", p2)
+
 	fmt.Println("Time", time.Since(startTime))
 }
 
-func part1(reactions []reaction, fuelNeeded int) int {
+func part1(reactions []reaction, fuelNeeded int64) int64 {
+	rmap := make(map[string]reaction)
+	for _, r := range reactions {
+		rmap[r.output.name] = r
+	}
+	needed := make(map[string]int64)
+	provided := make(map[string]int64)
+
+	fuel := rmap["FUEL"]
+	needed[fuel.output.name] = fuelNeeded
+
+	fulfill(fuel.output, fuelNeeded, rmap, provided, needed)
+
+	return provided["ORE"]
+}
+
+func part2(reactions []reaction, oreProvided int64) int64 {
 	rmap := make(map[string]reaction)
 	for _, r := range reactions {
 		rmap[r.output.name] = r
 	}
 
-	needed := make(map[string]int)
+	// needed := make(map[string]int64)
+	provided := make(map[string]int64)
 
-	fuel, _ := rmap["FUEL"]
-	provided := make(map[string]int)
+	provided["ORE"] = oreProvided
+	provided["FUEL"] = 0
 
-	fulfill(fuel.output, 1, rmap, provided, needed)
+	var total int64
+	pfuel := int64(1)
+	for provided["ORE"] > 0 {
+		total += pfuel
+		fulfill2(rmap["FUEL"].output, pfuel, rmap, provided)
+		if provided["ORE"]%100000 == 0 {
+			fmt.Println(provided["ORE"])
+		}
 
-	return provided["ORE"]
+		if provided["ORE"] < 1000 {
+			fmt.Println("less then 1 thousand", provided["ORE"])
+			pfuel = 1
+		} else if provided["ORE"] < 100_000 {
+			fmt.Println("less then 100 thousand", provided["ORE"])
+			pfuel = 1
+		} else if provided["ORE"] < 100_000_000 {
+			fmt.Println("less then 10 million", provided["ORE"])
+			pfuel = 100
+		}
+	}
+
+	return total
 }
 
-func fulfill(chem chemical, count int, rmap map[string]reaction, provided map[string]int, needed map[string]int) {
+func fulfill2(chem chemical, count int64, rmap map[string]reaction, provided map[string]int64) {
+	// fmt.Println(chem.name, count)
 	r := rmap[chem.name]
-
 	for _, i := range r.input {
-		needed[i.name] += i.count
+		re, ok := rmap[i.name]
+		var c int64 = count
+		for provided[i.name] < count {
+			fulfill2(i, i.count*count, rmap, provided)
+			if ok {
+				c = re.output.count * count
+				provided[i.name] += c
+			} else {
+				c = i.count * count
+				provided[i.name] += c
+			}
+		}
+		provided[i.name] -= c
+	}
+}
+
+func fulfill(chem chemical, count int64, rmap map[string]reaction, provided map[string]int64, needed map[string]int64) {
+	r := rmap[chem.name]
+	for _, i := range r.input {
+		needed[i.name] += i.count * count
 		re, ok := rmap[i.name]
 		for provided[i.name] < needed[i.name] {
-			fulfill(i, i.count, rmap, provided, needed)
+			fulfill(i, count, rmap, provided, needed)
 			if ok {
-				provided[i.name] += re.output.count
+				provided[i.name] += re.output.count * count
 			} else {
-				provided[i.name] += i.count
+				provided[i.name] += i.count * count
 			}
 		}
 	}
