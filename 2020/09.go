41b3412cb11de1a19b2e727783cf5759e617f6dc
Jason Connell <jconnell@delphicdigital.com>
2020-12-09 09:09:54 -0500

    2020 day 9 part 2

--- 2020/09.go
+++ 2020/09.go
@@ -4,16 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"time"
 )
 
-var input = "09.txt"
+var input = "09_test.txt"
+
+type infile struct {
+	filename string
+	preamble int
+}
 
 func main() {
 	startTime := time.Now()
 
-	f, err := os.Open(input)
+	infile1 := infile{"09.txt", 25}
+	infile2 := infile{"09_test.txt", 5}
+
+	inmap := make(map[string]infile)
+
+	inmap["main"] = infile1
+	inmap["test"] = infile2
+
+	file := inmap["main"]
+
+	f, err := os.Open(file.filename)
 
 	if err != nil {
 		fmt.Println(err)
@@ -33,14 +49,23 @@ func main() {
 		nums = append(nums, i)
 	}
 
-	p1 := part1(nums, 25)
+	p1 := firstInvalidNumber(nums, file.preamble)
 	fmt.Println("part 1:", p1)
+
+	a := findContiguousSum(p1, nums)
+	sort.Ints(a)
+
+	p2 := -1
+	if a != nil && len(a) > 1 {
+		p2 = a[0] + a[len(a)-1]
+	}
+	fmt.Println("Part 2:", p2)
 
 	fmt.Println("read", len(nums), "numbers")
 	fmt.Println("Time", time.Since(startTime))
 }
 
-func part1(nums []int, size int) int {
+func firstInvalidNumber(nums []int, size int) int {
 	p1 := 0
 	for i := size; i < len(nums); i++ {
 		preamble := getPreamble(nums, i-size, size)
@@ -50,6 +75,41 @@ func part1(nums []int, size int) int {
 		}
 	}
 	return p1
+}
+
+func findContiguousSum(num int, nums []int) []int {
+	q := []int{}
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == num {
+			continue
+		}
+
+		if nums[i] > num {
+			break
+		}
+
+		s := sum(q)
+		for s > num {
+			q = q[1:]
+			s = sum(q)
+		}
+
+		if s == num {
+			return q
+		}
+
+		q = append(q, nums[i])
+
+	}
+	return nil
+}
+
+func sum(a []int) int {
+	s := 0
+	for _, n := range a {
+		s += n
+	}
+	return s
 }
 
 func hasMatch(num int, preamble map[int]int) bool {
