83de4912869a2162448b64704bf95624b19244dd
Jason Connell <jconnell@delphicdigital.com>
2020-12-14 09:51:25 -0500

    2020 day 13 part 2

--- 2020/13.go
+++ 2020/13.go
@@ -38,8 +38,11 @@ func main() {
 	sched := getSchedule(lines)
 
 	p1 := firstBus(sched)
+	p2 := matchTimestamp(sched)
 
 	fmt.Println("Part 1:", p1)
+
+	fmt.Println("Part 2:", p2)
 	fmt.Println("Time", time.Since(startTime))
 }
 
@@ -62,8 +65,71 @@ func firstBus(sched schedule) int {
 	return bid * min
 }
 
-func matchTimestamp(sched schedule) int {
-	return 0
+func matchesTimestamp(ts, pos, bus int64) bool {
+	b := (ts+pos)%bus == 0
+	return b
+}
+
+func matchTimestamp(sched schedule) int64 {
+	m := make(map[int64]int64)
+	indices := []int64{}
+
+	for i, b := range sched.buses {
+		if b == -1 {
+			continue
+		}
+		m[int64(i)] = int64(b)
+		indices = append(indices, int64(i))
+	}
+
+	done := false
+	var factor int64 = 1
+	var factorStep int64 = 1
+	var x int64
+	var loops int64
+
+	lasts := make(map[int]int64)
+	maxes := make(map[int]int64)
+
+	for !done {
+		allFound := true
+		x = m[indices[0]] * factor
+		for idx, i := range indices[1:] {
+			b := m[i]
+			if (x+i)%b != 0 {
+				allFound = false
+				break
+			} else if idx > 0 {
+				if cmax, ok := maxes[idx]; ok {
+					last, lok := lasts[idx]
+					if lok {
+						if factor-last > cmax {
+							maxes[idx] = factor - last
+
+							var nmax int64
+							for _, mx := range maxes {
+								if mx > nmax {
+									nmax = mx
+								}
+							}
+							factorStep = nmax
+						}
+					}
+
+					lasts[idx] = factor
+				} else {
+					maxes[idx] = factor
+					lasts[idx] = factor
+				}
+			}
+		}
+		done = allFound
+
+		factor += factorStep
+		loops++
+	}
+
+	return x
 }
 
 func getSchedule(lines []string) schedule {
