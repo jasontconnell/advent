02729ce0b87b55d998d628817ea058e3b2e6328a
Jason Connell <jconnell@delphicdigital.com>
2020-12-29 15:49:48 -0500

    2020 Day 22 Part 2

--- 2020/22.go
+++ 2020/22.go
@@ -8,10 +8,19 @@ import (
 	"time"
 )
 
-var input = "22_test.txt"
+var input = "22.txt"
 
 type deck struct {
+	id    int
 	cards []int
+}
+
+func cardstr(cards []int) string {
+	s := ""
+	for _, i := range cards {
+		s += strconv.Itoa(i) + "_"
+	}
+	return s
 }
 
 func main() {
@@ -36,17 +45,20 @@ func main() {
 	winner := play(player1, player2)
 	p1 := getDeckValue(winner)
 
-	fmt.Println("Part 1:", p1)
+	fmt.Println("Part 1: Player", winner.id, "wins! Score:", p1)
 
 	p2winner := playRecursiveCombat(player1, player2)
 	p2 := getDeckValue(p2winner)
 
-	fmt.Println("Part 2:", p2)
+	fmt.Println("Part 2: Player", p2winner.id, "wins! Score:", p2)
 
 	fmt.Println("Time", time.Since(startTime))
 }
 
 func getDeckValue(d deck) int {
+	if len(d.cards) == 0 {
+		return 0
+	}
 	c := 1
 	v := 0
 	for i := len(d.cards) - 1; i >= 0; i-- {
@@ -77,28 +89,10 @@ func play(player1, player2 deck) deck {
 }
 
 func playRecursiveCombat(player1, player2 deck) deck {
-	p1m := make(map[int]int)
-	p2m := make(map[int]int)
-
-	p1m[getDeckValue(player1)] = getDeckValue(player1)
-	p2m[getDeckValue(player2)] = getDeckValue(player2)
-
-	return recurse(player1, player2, p1m, p2m)
+	return recurse(player1, player2)
 }
 
-func recurse(player1, player2 deck, p1m map[int]int, p2m map[int]int) deck {
-	p1v := getDeckValue(player1)
-	p2v := getDeckValue(player2)
-
-	_, p1ok := p1m[p1v]
-	_, p2ok := p2m[p2v]
-
-	if p1ok || p2ok {
-		return player1
-	}
-
-	p1m[p1v] = p1v
-	p2m[p2v] = p2v
+func recurse(player1, player2 deck) deck {
 
 	if len(player1.cards) == 0 {
 		return player2
@@ -108,26 +102,82 @@ func recurse(player1, player2 deck, p1m map[int]int, p2m map[int]int) deck {
 		return player1
 	}
 
-	p1c := player1.cards[0]
-	p2c := player2.cards[0]
+	determined := false
+	p1m := make(map[int]int)
+	p2m := make(map[int]int)
 
-	player1.cards = player1.cards[1:]
-	player2.cards = player2.cards[1:]
+	var winner deck
+	for !determined {
 
-	if p1c < len(player1.cards) {
-		return player2
+		p1c := player1.cards[0]
+		p2c := player2.cards[0]
+
+		// fmt.Println("at start", player1, player2)
+
+		player1.cards = player1.cards[1:]
+		player2.cards = player2.cards[1:]
+
+		p1v := getDeckValue(player1)
+		p2v := getDeckValue(player2)
+
+		_, p1ok := p1m[p1v]
+		_, p2ok := p2m[p2v]
+
+		playRound := true
+		if p1ok && p2ok {
+			player1.cards = append(player1.cards, []int{p1c, p2c}...)
+			playRound = false
+		}
+
+		if p1v != 0 {
+			p1m[p1v] = p1v
+		}
+		if p2v != 0 {
+			p2m[p2v] = p1v
+		}
+
+		if playRound {
+			if p1c <= len(player1.cards) && p2c <= len(player2.cards) {
+				p1copy := copyDeck(player1, p1c)
+				p2copy := copyDeck(player2, p2c)
+				recurseWinner := recurse(p1copy, p2copy)
+				if recurseWinner.id == 1 {
+					player1.cards = append(player1.cards, []int{p1c, p2c}...)
+				} else if recurseWinner.id == 2 {
+					player2.cards = append(player2.cards, []int{p2c, p1c}...)
+				}
+			} else {
+				if p1c > p2c {
+					player1.cards = append(player1.cards, []int{p1c, p2c}...)
+				} else {
+					player2.cards = append(player2.cards, []int{p2c, p1c}...)
+				}
+			}
+		}
+
+		determined = len(player1.cards) == 0 || len(player2.cards) == 0
 	}
 
-	if p2c < len(player2.cards) {
-		return player1
+	if winner.id == 0 {
+		winner = player1
+		if len(winner.cards) == 0 {
+			winner = player2
+		}
 	}
 
-	return recurse(player1, player2, p1m, p2m)
+	return winner
+}
+
+func copyDeck(d deck, length int) deck {
+	dc := deck{id: d.id}
+	dc.cards = make([]int, length)
+	copy(dc.cards, d.cards[:length])
+	return dc
 }
 
 func readDecks(lines []string) (deck, deck) {
-	player1 := deck{}
-	player2 := deck{}
+	player1 := deck{id: 1}
+	player2 := deck{id: 2}
 
 	cur := &player1
 	for _, line := range lines {
@@ -146,10 +196,3 @@ func readDecks(lines []string) (deck, deck) {
 
 	return player1, player2
 }
-
-// reg := regexp.MustCompile("-?[0-9]+")
-/*
-if groups := reg.FindStringSubmatch(txt); groups != nil && len(groups) > 1 {
-				fmt.Println(groups[1:])
-			}
-*/
