41a45b1e7beafab01cff072d7f908c53edd02188
Jason Connell <jasontconnell@gmail.com>
2022-12-08 06:33:23 -0500

    2022 Day 8 Part 1

---
+++ 2022/08/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/jasontconnell/advent/common"
+)
+
+type input = []string
+type output = int
+
+func main() {
+	startTime := time.Now()
+
+	in, err := common.ReadStrings(common.InputFilename(os.Args))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p1 := part1(in)
+	p2 := part2(in)
+
+	w := common.TeeOutput(os.Stdout)
+	fmt.Fprintln(w, "--2022 day 08 solution--")
+	fmt.Fprintln(w, "Part 1:", p1)
+	fmt.Fprintln(w, "Part 2:", p2)
+	fmt.Println("Time", time.Since(startTime))
+}
+
+func part1(in input) output {
+	grid := parseInput(in)
+	return getVisible(grid)
+}
+
+func part2(in input) output {
+	return 0
+}
+
+func getVisible(grid [][]int) int {
+	total := 0
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			if isVisible(grid, x, y) {
+				total++
+			}
+		}
+	}
+	return total
+}
+
+func isVisible(grid [][]int, x, y int) bool {
+	if x == 0 || y == 0 || y == len(grid)-1 || x == len(grid[y])-1 {
+		return true
+	}
+
+	var visleft, visright, visup, visdown bool = true, true, true, true
+	val := grid[y][x]
+	for i := 0; i < y; i++ {
+		if grid[i][x] >= val {
+			visup = false
+			break
+		}
+	}
+
+	for i := y + 1; i < len(grid); i++ {
+		if grid[i][x] >= val {
+			visdown = false
+			break
+		}
+	}
+
+	for i := 0; i < x; i++ {
+		if grid[y][i] >= val {
+			visleft = false
+			break
+		}
+	}
+
+	for i := x + 1; i < len(grid[y]); i++ {
+		if grid[y][i] >= val {
+			visright = false
+			break
+		}
+	}
+	return visdown || visup || visleft || visright
+}
+
+func parseInput(in input) [][]int {
+	grid := [][]int{}
+	for _, line := range in {
+		a := []int{}
+		for _, c := range line {
+			x, _ := strconv.Atoi(string(c))
+			a = append(a, x)
+		}
+		grid = append(grid, a)
+	}
+	return grid
+}
