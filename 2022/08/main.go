112a07c654d1860a817b88eb16f7fa803cb4c1cb
Jason Connell <jasontconnell@gmail.com>
2022-12-08 06:48:59 -0500

    2022 Day 8 Part 2

--- 2022/08/main.go
+++ 2022/08/main.go
@@ -37,7 +37,18 @@ func part1(in input) output {
 }
 
 func part2(in input) output {
-	return 0
+	grid := parseInput(in)
+	scores := calcScenicScores(grid)
+
+	high := 0
+	for _, r := range scores {
+		for _, c := range r {
+			if c > high {
+				high = c
+			}
+		}
+	}
+	return high
 }
 
 func getVisible(grid [][]int) int {
@@ -87,6 +98,52 @@ func isVisible(grid [][]int, x, y int) bool {
 		}
 	}
 	return visdown || visup || visleft || visright
+}
+
+func calcScenicScores(grid [][]int) [][]int {
+	scores := [][]int{}
+	for y := 1; y < len(grid)-1; y++ { // don't care about edges
+		row := []int{}
+		for x := 1; x < len(grid)-1; x++ {
+			row = append(row, getScenicScore(grid, x, y))
+		}
+		scores = append(scores, row)
+	}
+	return scores
+}
+
+func getScenicScore(grid [][]int, x, y int) int {
+	var left, right, up, down int = 0, 0, 0, 0
+	val := grid[y][x]
+	for i := y - 1; i >= 0; i-- {
+		up++
+		if grid[i][x] >= val {
+			break
+		}
+	}
+
+	for i := y + 1; i < len(grid); i++ {
+		down++
+		if grid[i][x] >= val {
+			break
+		}
+	}
+
+	for i := x - 1; i >= 0; i-- {
+		left++
+		if grid[y][i] >= val {
+			break
+		}
+	}
+
+	for i := x + 1; i < len(grid); i++ {
+		right++
+		if grid[y][i] >= val {
+			break
+		}
+	}
+
+	return left * right * up * down
 }
 
 func parseInput(in input) [][]int {
