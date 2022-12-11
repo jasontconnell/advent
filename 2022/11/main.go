2f92f95463d02b93ef28b97637bdc06f883988be
Jason Connell <jason.connell@herodigital.com>
2022-12-11 10:21:43 -0500

    2022 Day 11 part 2

--- 2022/11/main.go
+++ 2022/11/main.go
@@ -49,7 +49,25 @@ func main() {
 func part1(in input) output {
 	monkeys := parseInput(in)
 	for i := 0; i < 20; i++ {
-		monkeys = runRound(monkeys)
+		monkeys = runRound(monkeys, func(i int) int { return i / 3 })
+	}
+	insp := []int{}
+	for _, m := range monkeys {
+		insp = append(insp, m.inspected)
+	}
+	sort.Ints(insp)
+	return insp[len(insp)-1] * insp[len(insp)-2]
+
+}
+
+func part2(in input) output {
+	monkeys := parseInput(in)
+	mod := 1
+	for _, m := range monkeys {
+		mod = lcm(mod, m.testmod)
+	}
+	for i := 0; i < 10000; i++ {
+		monkeys = runRound(monkeys, func(i int) int { return i % mod })
 	}
 	insp := []int{}
 	for _, m := range monkeys {
@@ -59,26 +77,38 @@ func part1(in input) output {
 	return insp[len(insp)-1] * insp[len(insp)-2]
 }
 
-func part2(in input) output {
-	return 0
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+
+	return a
 }
 
-func runRound(monkeys []monkey) []monkey {
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func runRound(monkeys []monkey, worrymetohd func(i int) int) []monkey {
 	for i := 0; i < len(monkeys); i++ {
 		// fmt.Println("monkey", i, "items", monkeys[i].items)
+		monkeys[i].inspected += len(monkeys[i].items)
 		for len(monkeys[i].items) > 0 {
 			item := monkeys[i].items[0]
+			monkeys[i].items = monkeys[i].items[1:]
+
 			prm := monkeys[i].opparam
 			if monkeys[i].opparamself {
 				prm = item
 			}
+
 			if monkeys[i].op == "+" {
 				item = item + prm
 			} else {
 				item = item * prm
 			}
 
-			item = item / 3
+			item = worrymetohd(item)
 
 			if item%monkeys[i].testmod == 0 {
 				monkeys[monkeys[i].truedest].items = append(monkeys[monkeys[i].truedest].items, item)
@@ -87,9 +117,6 @@ func runRound(monkeys []monkey) []monkey {
 				monkeys[monkeys[i].falsedest].items = append(monkeys[monkeys[i].falsedest].items, item)
 				// fmt.Println("false", item, monkeys[i].testmod, "thrown to", monkeys[i].falsedest, monkeys[monkeys[i].falsedest].items)
 			}
-
-			monkeys[i].inspected++
-			monkeys[i].items = monkeys[i].items[1:]
 		}
 	}
 	return monkeys
