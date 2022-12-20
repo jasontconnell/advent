64f383c86631835a8d241c6b90dff67146f295d1
Jason Connell <jasontconnell@gmail.com>
2022-12-20 01:57:02 -0500

    2022 Day 20 part 1

---
+++ 2022/20/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jasontconnell/advent/common"
+)
+
+type input = []int
+type output = int
+
+type node struct {
+	value       int
+	moved       bool
+	left, right *node
+	origidx     int
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("[%d] = %d moved: %t", n.origidx, n.value, n.moved)
+}
+
+func print(front *node, ptr *node) {
+	cur := front
+	first := true
+	for cur != front || first {
+		first = false
+		f := "%d"
+		if cur == ptr {
+			f = "[%d]"
+		}
+		fmt.Print(fmt.Sprintf(f, cur.value))
+		if cur.right != front {
+			fmt.Print(" -> ")
+		}
+		cur = cur.right
+	}
+	fmt.Println()
+}
+
+func main() {
+	startTime := time.Now()
+
+	in, err := common.ReadInts(common.InputFilename(os.Args))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p1 := part1(in)
+	p2 := part2(in)
+
+	w := common.TeeOutput(os.Stdout)
+	fmt.Fprintln(w, "--2022 day 20 solution--")
+	fmt.Fprintln(w, "Part 1:", p1)
+	fmt.Fprintln(w, "Part 2:", p2)
+	fmt.Println("Time", time.Since(startTime))
+}
+
+func part1(in input) output {
+	front := getList(in)
+	mix(front, len(in))
+	return getGroveCoordinates(front)
+}
+
+func part2(in input) output {
+	return 0
+}
+
+func mix(front *node, count int) {
+	cur := front
+	ptr := front
+	mvcount := 0
+	for mvcount < count {
+		tomove := cur.value
+		dir := 1
+		if tomove < 0 {
+			dir = -1
+			tomove *= -1
+		}
+
+		ptr = cur.right
+		cur.moved = true
+		mvcount++
+
+		for j := 0; j < tomove; j++ {
+			if dir == 1 {
+				tmpleft := cur.left
+				tmpright := cur.right
+				tmpright.left = tmpleft
+				tmpleft.right = tmpright
+
+				cur.right = tmpright.right
+				cur.left = tmpright
+				tmpright.right = cur
+				cur.right.left = cur
+
+			} else {
+				tmpleft := cur.left
+				tmpright := cur.right
+				tmpright.left = tmpleft
+				tmpleft.right = tmpright
+
+				cur.left = tmpleft.left
+				cur.right = tmpleft
+				tmpleft.left = cur
+				cur.left.right = cur
+			}
+		}
+		for mvcount < count && ptr.moved {
+			ptr = ptr.right
+		}
+		cur = ptr
+	}
+}
+
+func getGroveCoordinates(front *node) int {
+	gc := []int{}
+
+	cur := front
+	for cur.value != 0 {
+		cur = cur.right
+	}
+
+	for len(gc) < 3 {
+		for i := 0; i < 1000; i++ {
+			cur = cur.right
+		}
+		gc = append(gc, cur.value)
+	}
+	return gc[0] + gc[1] + gc[2]
+}
+
+func getList(in input) *node {
+	var front, prev *node
+	for i := 0; i < len(in); i++ {
+		if i == 0 {
+			front = &node{value: in[i], origidx: i}
+			prev = front
+			continue
+		}
+
+		n := &node{value: in[i], left: prev, origidx: i}
+		prev.right = n
+		prev = n
+	}
+	front.left = prev
+	prev.right = front
+	return front
+}
