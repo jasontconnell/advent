4dec1356fd95d7666bfd0701fd324265d26de931
Jason Connell <jasontconnell@gmail.com>
2023-12-11 01:26:53 -0500

    Saving day 10 work.

--- 2023/10/main.go
+++ 2023/10/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
 	"os"
 
 	"github.com/jasontconnell/advent/common"
@@ -16,20 +15,35 @@ type xy struct {
 	x, y int
 }
 
-type pipe struct {
-	pt xy
-	ch rune
+func (pt xy) add(pt2 xy) xy {
+	return xy{pt.x + pt2.x, pt.y + pt2.y}
 }
 
-type state struct {
-	pt       xy
-	steps    int
-	distance int
+func (pt xy) sub(pt2 xy) xy {
+	return xy{pt2.x - pt.x, pt2.y - pt.y}
+}
+
+func (pt xy) opposite() xy {
+	p2 := xy{-pt.x, -pt.y}
+	return p2
+}
+
+func (pt xy) eq(p2 xy) bool {
+	return pt.x == p2.x && pt.y == p2.y
+}
+
+type pipe struct {
+	pt     xy
+	ch     rune
+	dir    xy
+	outdir xy
 }
 
 func (p pipe) String() string {
 	return fmt.Sprintf("(%d,%d) %c", p.pt.x, p.pt.y, p.ch)
 }
+
+var dirs []xy = []xy{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
 func main() {
 	in, err := common.ReadStrings(common.InputFilename(os.Args))
@@ -53,85 +67,145 @@ func part1(in input) output {
 }
 
 func part2(in input) output {
+	start, pipes := parseInput(in)
+	list := getLoopPoints(start, pipes)
+	markOuter(list, pipes)
 	return 0
 }
 
 func traverse(start pipe, pipes map[xy]pipe) int {
-	maxstate := state{pt: xy{}, steps: 0, distance: 0}
+	list := getLoopPoints(start, pipes)
+	return len(list) / 2
+}
 
+func markOuter(list []pipe, pipes map[xy]pipe) {
+	outer := true // mark right side outer
+	initial := false
+	for i := 1; i < len(list); i++ {
+		cur := list[i]
+		prev := list[i-1]
+
+		if !initial {
+			odir := xy{0, 1}
+			switch prev.dir {
+			case xy{0, 1}:
+				odir = xy{-1, 0}
+				if outer {
+					odir = xy{1, 0}
+				}
+			case xy{0, -1}:
+				odir = xy{1, 0}
+				if outer {
+					odir = xy{-1, 0}
+				}
+			case xy{1, 0}:
+				odir = xy{0, -1}
+				if outer {
+					odir = xy{0, 1}
+				}
+			case xy{-1, 0}:
+				odir = xy{0, 1}
+				if outer {
+					odir = xy{0, -1}
+				}
+			}
+			prev.outdir = odir
+			initial = true
+		}
+
+		if prev.dir.eq(cur.dir) {
+			cur.outdir = prev.outdir
+		}
+	}
+	fmt.Println(initial, outer)
+}
+
+// a point is enclosed if it can't reach the outside
+// a point is not enclosed if it can reach another not enclosed point
+func floodfill(pt xy, pipes map[xy]pipe, loop []pipe, enclosed map[xy]bool) {
 	v := make(map[xy]bool)
-	v[start.pt] = true
-
-	var cur state
-	queue := []state{{pt: start.pt, steps: 0}}
-	for len(queue) > 0 {
-		cur = queue[0]
-		queue = queue[1:]
-
-		v[cur.pt] = true
-
-		if cur.steps > maxstate.steps {
-			maxstate.distance = cur.distance
-			maxstate.steps = cur.steps
-			maxstate.pt = cur.pt
-		}
-
-		mvs := getMoves(cur.pt, pipes)
-		for _, mv := range mvs {
-			if _, ok := v[mv]; ok {
-				continue
-			}
-			d := distance(mv, start.pt)
-
-			queue = append(queue, state{pt: mv, steps: cur.steps + 1, distance: d})
-		}
-	}
-	return maxstate.steps
+	fmt.Println(v)
 }
 
-func distance(p1, p2 xy) int {
-	d := math.Abs(float64(p2.y-p1.y)) + math.Abs(float64(p2.x-p1.x))
-	return int(d)
-}
+func getLoopPoints(start pipe, pipes map[xy]pipe) []pipe {
+	list := []pipe{}
+	cur := start.pt
+	dir := xy{0, 0}
+	done := false
+	for !done {
+		p, ok := pipes[cur]
+		if !ok {
+			break
+		}
 
-func getMoves(pt xy, pipes map[xy]pipe) []xy {
-	p := pipes[pt]
-	mvs := []xy{}
-
-	if p.ch == 'S' {
-		for y := pt.y - 1; y <= pt.y+1; y++ {
-			for x := pt.x - 1; x <= pt.x+1; x++ {
-				p2 := xy{x, y}
-				if p2 == pt {
-					continue
-				}
-
-				smvs := getMoves(p2, pipes)
-				for _, smv := range smvs {
-					if smv == pt {
-						mvs = append(mvs, p2)
-					}
+		var np xy
+		switch p.ch {
+		case '|':
+			if dir.eq(xy{0, 1}) || dir.eq(xy{0, -1}) {
+				np = cur.add(dir) // keep going in same dir
+			}
+		case '-':
+			if dir.eq(xy{-1, 0}) || dir.eq(xy{1, 0}) {
+				np = cur.add(dir)
+			}
+		case 'L':
+			if dir.eq(xy{0, 1}) {
+				dir = xy{1, 0}
+			} else if dir.eq(xy{-1, 0}) {
+				dir = xy{0, -1}
+			}
+			np = cur.add(dir)
+		case 'J':
+			if dir.eq(xy{0, 1}) {
+				dir = xy{-1, 0}
+			} else if dir.eq(xy{1, 0}) {
+				dir = xy{0, -1}
+			}
+			np = cur.add(dir)
+		case '7':
+			if dir.eq(xy{0, -1}) {
+				dir = xy{-1, 0}
+			} else if dir.eq(xy{1, 0}) {
+				dir = xy{0, 1}
+			}
+			np = cur.add(dir)
+		case 'F':
+			if dir.eq(xy{0, -1}) {
+				dir = xy{1, 0}
+			} else if dir.eq(xy{-1, 0}) {
+				dir = xy{0, 1}
+			}
+			np = cur.add(dir)
+		case 'S':
+			dir = xy{0, 0}
+			for _, d := range dirs {
+				cp := p.pt.add(d)
+				dest := pipes[cp]
+				// first one wins
+				if d.eq(xy{0, 1}) && (dest.ch == '|' || dest.ch == 'J' || dest.ch == 'L') {
+					dir = d
+					break
+				} else if d.eq(xy{0, -1}) && (dest.ch == '|' || dest.ch == 'F' || dest.ch == '7') {
+					dir = d
+					break
+				} else if d.eq(xy{-1, 0}) && (dest.ch == '-' || dest.ch == 'F' || dest.ch == 'L') {
+					dir = d
+					break
+				} else if d.eq(xy{1, 0}) && (dest.ch == '-' || dest.ch == 'J' || dest.ch == '7') {
+					dir = d
+					break
 				}
 			}
+			np = cur.add(dir)
 		}
-		return mvs
-	}
 
-	switch p.ch {
-	case '|':
-		mvs = append(mvs, xy{pt.x, pt.y - 1}, xy{pt.x, pt.y + 1})
-	case '-':
-		mvs = append(mvs, xy{pt.x - 1, pt.y}, xy{pt.x + 1, pt.y})
-	case 'L':
-		mvs = append(mvs, xy{pt.x + 1, pt.y}, xy{pt.x, pt.y - 1})
-	case 'J':
-		mvs = append(mvs, xy{pt.x, pt.y - 1}, xy{pt.x - 1, pt.y})
-	case '7':
-		mvs = append(mvs, xy{pt.x - 1, pt.y}, xy{pt.x, pt.y + 1})
-	case 'F':
-		mvs = append(mvs, xy{pt.x, pt.y + 1}, xy{pt.x + 1, pt.y})
+		p.dir = dir
+		list = append(list, p)
+		cur = np
+
+		done = cur == start.pt
 	}
-	return mvs
+	return list // list will have start at the front and back
 }
 
 func parseInput(in input) (pipe, map[xy]pipe) {
@@ -147,5 +221,6 @@ func parseInput(in input) (pipe, map[xy]pipe) {
 			}
 		}
 	}
+
 	return start, pipes
 }
