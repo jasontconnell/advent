0797cb3bfc0992669651938d9ed4a7510cf8241b
Jason Connell <jasontconnell@gmail.com>
2023-12-15 01:04:14 -0500

    2023 Day 15 Part 2

--- 2023/15/main.go
+++ 2023/15/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/jasontconnell/advent/common"
@@ -12,9 +13,15 @@ import (
 type input = string
 type output = int
 
-type result struct {
-	orig string
-	val  int
+type lens struct {
+	orig     string
+	label    string
+	val      int
+	focallen int
+}
+
+type box struct {
+	lenses []lens
 }
 
 func main() {
@@ -39,10 +46,78 @@ func part1(in input) output {
 }
 
 func part2(in input) output {
-	return 0
+	steps := parseInput(in)
+	lenses := compute(steps)
+	m := initialize(lenses)
+	return calcFocalLen(m)
 }
 
-func sum(results []result) int {
+func calcFocalLen(m map[int]box) int {
+	total := 0
+	for i := 0; i < 256; i++ {
+		if _, ok := m[i]; !ok {
+			continue
+		}
+		c := i + 1
+		list := m[i].lenses
+		for j := 0; j < len(list); j++ {
+			x := c * ((j + 1) * list[j].focallen)
+			total += x
+		}
+	}
+	return total
+}
+
+func initialize(lenses []lens) map[int]box {
+	m := make(map[int]box)
+	for _, litr := range lenses {
+		dash := strings.Index(litr.orig, "-")
+		eq := strings.Index(litr.orig, "=")
+
+		if dash > 0 {
+			lbl := litr.orig[:dash]
+			litr.label = lbl
+			hash := computeOne(lbl).val
+			if _, ok := m[hash]; ok && len(m[hash].lenses) > 0 {
+				box := m[hash]
+				for i := len(box.lenses) - 1; i >= 0; i-- {
+					if box.lenses[i].label == lbl {
+						box.lenses = append(box.lenses[:i], box.lenses[i+1:]...)
+					}
+				}
+				m[hash] = box
+			}
+		} else if eq > 0 {
+			sp := strings.Split(litr.orig, "=")
+			lbl := sp[0]
+			length, _ := strconv.Atoi(sp[1])
+			hash := computeOne(lbl).val
+
+			litr.label = lbl
+			litr.focallen = length
+			if _, ok := m[hash]; ok {
+				box := m[hash]
+				found := false
+				for i := 0; i < len(box.lenses); i++ {
+					if box.lenses[i].label == lbl {
+						box.lenses[i] = litr
+						found = true
+					}
+				}
+				if !found {
+					box.lenses = append(box.lenses, litr)
+				}
+				m[hash] = box
+			} else {
+				b := box{lenses: []lens{litr}}
+				m[hash] = b
+			}
+		}
+	}
+	return m
+}
+
+func sum(results []lens) int {
 	s := 0
 	for _, r := range results {
 		s += r.val
@@ -50,17 +125,17 @@ func sum(results []result) int {
 	return s
 }
 
-func compute(steps []string) []result {
-	results := []result{}
+func compute(steps []string) []lens {
+	lenses := []lens{}
 	for _, step := range steps {
 		r := computeOne(step)
-		results = append(results, r)
+		lenses = append(lenses, r)
 	}
-	return results
+	return lenses
 }
 
-func computeOne(step string) result {
-	r := result{orig: step}
+func computeOne(step string) lens {
+	r := lens{orig: step}
 	for _, c := range step {
 		r.val += int(c)
 		r.val *= 17
