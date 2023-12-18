c1247333ece23ddfe7a583316b65038c344cdcaf
Jason Connell <jasontconnell@gmail.com>
2023-12-17 23:13:44 -0500

    Insert into priority queue with binary search instead of linear search

--- common/genericheap_test.go
+++ common/genericheap_test.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"fmt"
 	"math/rand"
 	"sort"
 	"testing"
@@ -21,7 +22,50 @@ func TestDequeue(t *testing.T) {
 		item := q.Dequeue()
 		t.Log(item)
 	}
+}
 
+func TestBinarySearch(t *testing.T) {
+	vals := []int{1, 3, 5, 6, 9, 12, 15, 16, 18, 20, 24, 29, 31, 33, 43}
+
+	ins := 11
+	sort.Ints(vals)
+
+	idx := -1
+	if len(vals) > 0 && vals[0] > ins {
+		idx = 0
+	} else if len(vals) > 0 && vals[len(vals)-1] > ins {
+		lbound, ubound := 0, len(vals)
+		found := false
+		for !found {
+			idx = (lbound + ubound) / 2
+			midval := vals[idx]
+			if ins > midval {
+				lbound = idx
+			} else {
+				ubound = idx
+			}
+
+			fmt.Println(lbound, ubound)
+
+			if ubound-lbound <= 1 {
+				if ins > vals[lbound] {
+					idx = ubound
+				}
+				fmt.Println(lbound, vals[lbound], ubound, vals[ubound])
+				found = true
+			}
+		}
+	}
+
+	fmt.Println(idx)
+	fmt.Println(ins, vals)
+
+	if idx == -1 {
+		vals = append(vals, ins)
+	} else {
+		vals = append(vals[:idx], append([]int{ins}, vals[idx:]...)...)
+	}
+	fmt.Println("inserted", vals)
 }
 
 func TestHeap(t *testing.T) {
