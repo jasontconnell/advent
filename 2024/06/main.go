522ccd65ea8973be1c56993c49aa7bcc83e7f9b1
Jason Connell <jasontconnell@gmail.com>
2024-12-06 00:24:41 -0500

    2024 Day 6 part 1

---
+++ 2024/06/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+
+	"github.com/jasontconnell/advent/common"
+)
+
+type input = []string
+type output = int
+
+type dir int
+
+const (
+	E dir = iota
+	W
+	N
+	S
+)
+
+var turndir map[dir]xy = map[dir]xy{
+	E: {1, 0}, W: {-1, 0}, S: {0, 1}, N: {0, -1},
+}
+
+type xy struct {
+	x, y int
+}
+
+func (p xy) add(p2 xy) xy {
+	return xy{p.x + p2.x, p.y + p2.y}
+}
+
+type block struct {
+	wall bool
+}
+
+func main() {
+	in, err := common.ReadStrings(common.InputFilename(os.Args))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p1, p1time := common.Time(part1, in)
+	p2, p2time := common.Time(part2, in)
+
+	w := common.TeeOutput(os.Stdout)
+	fmt.Fprintln(w, "--2024 day 06 solution--")
+	fmt.Fprintln(w, "Part 1:", p1)
+	fmt.Fprintln(w, "Part 2:", p2)
+	fmt.Printf("Time %v (%v, %v)", p1time+p2time, p1time, p2time)
+}
+
+func part1(in input) output {
+	grid, start, d := parse(in)
+	return patrol(grid, start, d)
+}
+
+func part2(in input) output {
+	return 0
+}
+
+func minmax(g map[xy]block) (xy, xy) {
+	min := xy{math.MaxInt32, math.MaxInt32}
+	max := xy{math.MinInt32, math.MinInt32}
+
+	for k := range g {
+		if k.x < min.x {
+			min.x = k.x
+		}
+		if k.y < min.y {
+			min.y = k.y
+		}
+		if k.x > max.x {
+			max.x = k.x
+		}
+		if k.y > max.y {
+			max.y = k.y
+		}
+	}
+	return min, max
+}
+
+func patrol(g map[xy]block, start xy, startdir dir) int {
+	visited := make(map[xy]bool)
+	min, max := minmax(g)
+	cur := start
+	d := startdir
+	for {
+		if cur.x < min.x || cur.y < min.y || cur.x > max.x || cur.y > max.y {
+			break
+		}
+
+		visited[cur] = true
+		test := cur.add(turndir[d])
+		if b, ok := g[test]; (ok && !b.wall) || !ok {
+			cur = test
+			continue
+		} else if b.wall {
+			d = turnRight(d)
+		}
+	}
+	return len(visited)
+}
+
+func turnRight(d dir) dir {
+	var nd dir
+	switch d {
+	case E:
+		nd = S
+	case W:
+		nd = N
+	case S:
+		nd = W
+	case N:
+		nd = E
+	}
+	return nd
+}
+
+func parse(in []string) (map[xy]block, xy, dir) {
+	m := make(map[xy]block)
+	var d dir
+	var start xy
+	for y := 0; y < len(in); y++ {
+		for x := 0; x < len(in[y]); x++ {
+			c := in[y][x]
+			m[xy{x, y}] = block{wall: c == '#'}
+
+			if c != '.' && c != '#' {
+				start = xy{x, y}
+				switch c {
+				case '<':
+					d = W
+				case '>':
+					d = E
+				case '^':
+					d = N
+				case 'v':
+					d = S
+				}
+			}
+		}
+	}
+	return m, start, d
+}
