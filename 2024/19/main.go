7321d8d8961d1dff7f773ed3983c148610b546f8
Jason Connell <jasontconnell@gmail.com>
2024-12-19 22:34:19 -0500

    2024 Day 19 part 2, rewrite

--- 2024/19/main.go
+++ 2024/19/main.go
@@ -14,14 +14,16 @@ type input = []string
 type output = int
 
 type state struct {
-	pos   int
-	sub   string
-	total string
+	pos    int
+	sub    string
+	total  string
+	pieces string
 }
 
 type statekey struct {
-	pos int
-	sub string
+	pos    int
+	sub    string
+	pieces string
 }
 
 func main() {
@@ -42,29 +44,34 @@ func main() {
 
 func part1(in input) output {
 	p, d := parse(in)
-	return findValidDesigns(p, d)
+	return findValidDesigns(p, d, false)
 }
 
 func part2(in input) output {
 	p, d := parse(in)
-	for _, design := range d {
-		countVariations(p, design)
-	}
-	return 0
+	return findValidDesigns(p, d, true)
 }
 
-func findValidDesigns(pmap map[string][]string, designs []string) int {
+func findValidDesigns(pmap map[string]bool, designs []string, countall bool) int {
+	lookup := make(map[string]int)
 	min, max := minmaxlen(pmap)
 	count := 0
 	for _, design := range designs {
-		if designValid(pmap, design, min, max) {
+		var c int
+		c, lookup = countVariations(pmap, design, min, max, len(design), lookup)
+		if c == 0 {
+			continue
+		}
+		if !countall && c > 1 {
 			count++
+		} else if x, ok := lookup[design]; ok && countall {
+			count += x
 		}
 	}
 	return count
 }
 
-func minmaxlen(pmap map[string][]string) (int, int) {
+func minmaxlen(pmap map[string]bool) (int, int) {
 	min, max := math.MaxInt32, math.MinInt32
 	for s := range pmap {
 		if len(s) < min {
@@ -77,92 +84,33 @@ func minmaxlen(pmap map[string][]string) (int, int) {
 	return min, max
 }
 
-func countVariations(pmap map[string][]string, design string) {
-	for k, v := range pmap {
-		for _, s := range v {
-			if strings.Contains(design, k) {
-				log.Println(k, "in design", design)
-				for k2 := range pmap {
-					if len(k) > len(k2) || k == k2 || len(k) == 1 {
-						continue
-					}
-				}
-			}
+func countVariations(pmap map[string]bool, design string, min, max, orig int, lookup map[string]int) (int, map[string]int) {
+	w, ok := lookup[design]
+	if ok {
+		return w, lookup
+	}
+
+	count := 0
+	for k := range pmap {
+		if k == design {
+			count++
+		} else if strings.HasPrefix(design, k) {
+			var local int
+			local, lookup = countVariations(pmap, design[len(k):], min, max, orig, lookup)
+			count += local
 		}
 	}
+	lookup[design] = count
+	return lookup[design], lookup
 }
 
-func designValid(pmap map[string][]string, design string, min, max int) bool {
-	valid := false
-	queue := common.NewQueue[state, int]()
-	initial := state{pos: 0, sub: "", total: ""}
-	visit := make(map[statekey]bool)
-	queue.Enqueue(initial)
-	for queue.Any() {
-		cur := queue.Dequeue()
-
-		sk := statekey{pos: cur.pos, sub: cur.sub}
-		if _, ok := visit[sk]; ok {
-			continue
-		}
-		visit[sk] = true
-
-		if cur.total == design {
-			valid = true
-			continue
-		}
-
-		mvs := getPossibles(design, cur, pmap, min, max)
-		for _, mv := range mvs {
-			queue.Enqueue(mv)
-		}
-	}
-	return valid
-}
-
-func getPossibles(design string, cur state, pmap map[string][]string, min, max int) []state {
-	list := []state{}
-	for i := min; i <= max; i++ {
-		if cur.pos+i > len(design) {
-			continue
-		}
-		sub := design[cur.pos : cur.pos+i]
-		if _, ok := pmap[sub]; ok {
-			list = append(list, state{pos: cur.pos + i, sub: sub, total: cur.total + sub})
-		}
-	}
-	return list
-}
-
-func parse(in []string) (map[string][]string, []string) {
+func parse(in []string) (map[string]bool, []string) {
 	patterns := strings.Fields(strings.ReplaceAll(in[0], ",", ""))
 	designs := in[2:]
-	pmap := make(map[string][]string)
+	pmap := make(map[string]bool)
 	for _, p := range patterns {
-		pmap[p] = []string{}
-	}
-	for k := range pmap {
-		subs := allSubstrings(k)
-		for _, s := range subs {
-			if _, ok := pmap[s]; ok {
-				pmap[s] = append(pmap[s], s)
-			}
-		}
+		pmap[p] = true
 	}
 
 	return pmap, designs
-}
-
-func allSubstrings(str string) []string {
-	list := []string{}
-	for i := 0; i < len(str); i++ {
-		for j := 0; j < len(str); j++ {
-			if i > j {
-				continue
-			}
-			v := str[i : j+1]
-			list = append(list, v)
-		}
-	}
-	return list
 }
